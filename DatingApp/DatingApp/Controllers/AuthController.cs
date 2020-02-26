af9bd1c7ea761d77e4c980384b3a05638a53440c
Nobel <nobelnurulislam@gmail.com>
2020-02-26 08:50:13 +0600

    tokenAuthorization

--- DatingApp/DatingApp/Controllers/AuthController.cs
+++ DatingApp/DatingApp/Controllers/AuthController.cs
@@ -1,31 +1,39 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
+using System.Text;
 using System.Threading.Tasks;
 using DatingApp.Data;
 using DatingApp.Dtos;
 using DatingApp.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
 
 namespace DatingApp.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthController:ControllerBase
+    public class AuthController : ControllerBase
     {
         private readonly IAuthRepository _context;
-        public AuthController(IAuthRepository context)
+
+        private readonly IConfiguration _config;
+        public AuthController(IAuthRepository context, IConfiguration config)
         {
             _context = context;
+            _config = config;
         }
 
         [HttpPost("register")]
         public async Task<IActionResult> register(UserForRegisterDto userforregisterdto)
         {
-            userforregisterdto.Username= userforregisterdto.Username.ToLower();
+            userforregisterdto.Username = userforregisterdto.Username.ToLower();
 
-            if(await _context.UserExists(userforregisterdto.Username))
+            if (await _context.UserExists(userforregisterdto.Username))
             {
                 return BadRequest("Username alreaady exists");
             }
@@ -34,10 +42,47 @@ namespace DatingApp.Controllers
 
             user.UserName = userforregisterdto.Username;
 
-            user =await _context.Register(user, userforregisterdto.Password);
+            user = await _context.Register(user, userforregisterdto.Password);
 
             return StatusCode(201);
 
         }
-    }
+
+        [HttpPost("login")]
+        public async Task<IActionResult> login(UserForLoginDto userforlogindto)
+        {
+            var UserFromRepo = await _context.Login(userforlogindto.Username, userforlogindto.Password);
+            if (UserFromRepo == null)
+            {
+                return Unauthorized();
+            }
+
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, UserFromRepo.Id.ToString()),
+                new Claim(ClaimTypes.Name, UserFromRepo.UserName)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_config.GetSection("AppSettings:Token").Value));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var TokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var TokenHandler = new JwtSecurityTokenHandler();
+
+            var token = TokenHandler.CreateToken(TokenDescriptor);
+
+            return Ok(new
+            {
+                token = TokenHandler.WriteToken(token)
+            });
+        }
+    }   
 }
+
