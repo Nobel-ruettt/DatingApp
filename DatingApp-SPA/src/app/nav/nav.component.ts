65cec283a1b71390379fe3c31530d90704bcf446
Nobel-ruettt <nobelnurulislam@gmail.com>
2020-03-21 22:36:55 +0600

    protecting routes with guards

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -1,6 +1,7 @@
 import { AlertifyService } from './../_services/alertify.service';
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../_services/auth.service';
+import { Router } from '@angular/router';
 
 
 @Component({
@@ -10,13 +11,15 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(public authService: AuthService, private alertify: AlertifyService) {}
+  constructor(public authService: AuthService, private alertify: AlertifyService,private router: Router) {}
   ngOnInit() {}
   login() {
       this.authService.login(this.model).subscribe(next => {
         this.alertify.success('Logged in Successfully');
       }, error => {
         this.alertify.error(error);
+      }, () => {
+        this.router.navigate(['/members']);
       });
   }
   loggedIn() {
@@ -26,5 +29,6 @@ export class NavComponent implements OnInit {
   logout() {
     localStorage.removeItem('token');
     this.alertify.message('Logged out');
+    this.router.navigate(['/home']);
   }
 }
