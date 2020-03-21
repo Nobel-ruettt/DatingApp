a3cb640574f7ab3c2f5bc3f91f72bb6d45c3709d
Nobel-ruettt <nobelnurulislam@gmail.com>
2020-03-21 15:01:52 +0600

    ngx bootstrap and jswttoken added

--- DatingApp-SPA/src/app/nav/nav.component.ts
+++ DatingApp-SPA/src/app/nav/nav.component.ts
@@ -10,7 +10,7 @@ import { AuthService } from '../_services/auth.service';
 })
 export class NavComponent implements OnInit {
   model: any = {};
-  constructor(private authService: AuthService, private alertify: AlertifyService) {}
+  constructor(public authService: AuthService, private alertify: AlertifyService) {}
   ngOnInit() {}
   login() {
       this.authService.login(this.model).subscribe(next => {
