71ac6bef848dee9cf4073a2f73359db34b290e7f
Nobel-ruettt <nobelnurulislam@gmail.com>
2020-03-27 05:34:33 +0600

    populating database

--- DatingApp/DatingApp/Data/DataContext.cs
+++ DatingApp/DatingApp/Data/DataContext.cs
@@ -14,5 +14,7 @@ namespace DatingApp.Data
 
         public DbSet<User> Users { get; set; }
 
+        public DbSet<Photo> Photos { get; set; }
+
     }
 }
---
+++ DatingApp/DatingApp/Data/Seed.cs
@@ -0,0 +1,41 @@
+﻿using DatingApp.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.Data
+{
+    public class Seed
+    {
+        public static void SeedUsers(DataContext context)
+        {
+            if(!context.Users.Any())
+            {
+                var userData = System.IO.File.ReadAllText("Data/UserSeedData.json");
+                var users = JsonConvert.DeserializeObject<List<User>>(userData);
+                foreach(var user in users)
+                {
+                    byte[] passwordhash, passwordSalt;
+                    CreatePassWordHash("password", out passwordhash, out passwordSalt);
+                    user.PassWordHash = passwordhash;
+                    user.PassWordSalt = passwordSalt;
+                    user.UserName = user.UserName.ToLower();
+                    context.Users.Add(user);
+                }
+                context.SaveChanges();
+            }
+        }
+
+        private static void CreatePassWordHash(string password, out byte[] passWordHash, out byte[] passWordSalt)
+        {
+            using (var hmac = new System.Security.Cryptography.HMACSHA512())
+            {
+                passWordSalt = hmac.Key;
+                passWordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password));
+            }
+
+        }
+    }
+}
